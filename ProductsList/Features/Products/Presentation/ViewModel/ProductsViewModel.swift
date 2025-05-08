ca7cc575d9691b491f98d5dcc53f252dfb9c8384
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-05-08 20:11:41 +0300

    add products view model and test the api call

---
+++ ProductsList/Features/Products/Presentation/ViewModel/ProductsViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  ProductsViewModel.swift
+//  ProductsList
+//
+//  Created by Yousuf Abdelfattah on 08/05/2025.
+//
+
+import Foundation
+import Combine
+import Factory
+
+class ProductsViewModel: BaseVM {
+    @Injected(\.getProductsUseCase) var getProductsUseCase
+}
+
+
+// MARK: API Calls
+
+extension ProductsViewModel {
+    func getProducts() {
+        let request = ProductsRequest(limit: 7)
+        
+        getProductsUseCase.execute(productsRequest: request) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+                case .success(let productsResponse):
+                    print("Success \(productsResponse)")
+                    
+                case .failure(let error):
+                    self.onFailure(error.localizedDescription)
+            }
+        }
+        
+    }
+}
